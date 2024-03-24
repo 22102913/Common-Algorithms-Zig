58f5d993191f2ca73ef81cecc79ce290066ffda6
22102913 <220993@student.wqe.ac.uk>
2024-03-24 22:15:32 +0000

    Added Binary search

--- src/main.zig
+++ src/main.zig
@@ -11,3 +11,15 @@ test "linearSearch" {
     const result = searches.LinearSearch(u8, item, &arr);
     try std.testing.expect(result == 9);
 }
+
+test "BinarySearch" {
+    const arr = [_]u8{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    const item: u8 = 12;
+    const result = searches.BinarySearch(u8, item, &arr);
+    try std.testing.expect(result == 12);
+
+    const arr1 = [_]u8{ 2, 4, 5, 7, 8, 9, 12, 34, 58, 63, 84, 92, 120 };
+    const item1: u8 = 84;
+    const result1 = searches.BinarySearch(u8, item1, &arr1);
+    try std.testing.expect(result1 == 10);
+}
--- src/searches.zig
+++ src/searches.zig
@@ -5,3 +5,22 @@ pub fn LinearSearch(T: type, item: T, arr: []const T) u64 {
     }
     unreachable;
 }
+
+pub fn BinarySearch(T: type, item: T, arr: []const T) u64 {
+    var lower: u64 = 0;
+    var higher: u64 = arr.len;
+    var mid: u64 = arr.len / 2;
+
+    while (true) {
+        if (lower > higher)
+            unreachable;
+        if (item == arr[mid]) {
+            return mid;
+        } else if (item < arr[mid]) {
+            higher = mid - 1;
+        } else if (item > arr[mid]) {
+            lower = mid + 1;
+        }
+        mid = (higher - lower) / 2 + lower;
+    }
+}
